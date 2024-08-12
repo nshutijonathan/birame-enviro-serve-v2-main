f7e2082b2309ae3aa5bb0669852a17d0fff512ec
Jonathan Nshuti <nshutijonathan130@gmail.com>
2024-08-12 19:24:04 +0200

    added live client id

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,14 @@ import Donate from './components/Donate/Donate';
 import Footer from './components/Footer/Footer';
 import Hello from './components/Hello/Hello';
 import Projects from './components/Projects/Projects';
-import Resources from './components/Resources/Resources';
 import Team from './components/Team/Team';
 // import WhatWeDo from './components/WhatWeDo/WhatWeDo';
-import WhatWeDo from './components/Resources/Resources';
 import Resource from './components/Resource/Resource';
+import WhatWeDo from './components/Resources/Resources';
 const App = () => {
   const initialOptions = {
     'client-id':
-      'AZvdle3o3OD7QtoG7cGhoeNqT8Ht4_Nl9FgA-pdutQkBpU-FQ9l6BW9FBaqYx4k2cmg4nsdR9gQzF-pl',
+      'AZmhCArmt7HzLV0vq1kx5wMdw8zfxK4Koh90f0FPEmnFH3vcj8xGWu7Fg_cTU4XFFkq3K2zqupbS6WtN',
     currency: 'USD',
     intent: 'capture',
   };
