f9ed4d57341800e8c07c90a0cf2565f5fcfecb6f
fdahaba <138160871+fadgadahababashirdev@users.noreply.github.com>
2025-03-03 14:30:34 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { PayPalScriptProvider } from '@paypal/react-paypal-js';
 import About from './components/About/About';
-import Donate from './components/Donate/Donate';
+
 import Footer from './components/Footer/Footer';
 import Hello from './components/Hello/Hello';
 import Projects from './components/Projects/Projects';
 import Team from './components/Team/Team';
 // import WhatWeDo from './components/WhatWeDo/WhatWeDo';
 import Resource from './components/Resource/Resource';
-import WhatWeDo from './components/Resources/Resources';
+import WhatWeDo from './components/Resources/Resources'; 
+import Donate from "./components/Donate/Donate"
 const App = () => {
   const envVariable = import.meta.env.VITE_API_URL;
   const initialOptions = {
@@ -24,7 +25,8 @@ const App = () => {
         <Projects />
         <Team />
         <Resource />
-        <Donate />
+        {/* donate */}
+        <Donate/>
         <Footer />
       {/* </PayPalScriptProvider> */}
     </>
